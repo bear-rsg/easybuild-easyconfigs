ea63d998bebbe2b45eafc0dd67d84a0df4afb4c2
Simon Branford <s.j.branford@bham.ac.uk>
2021-07-28 12:18:47 +0100

    Fix the RELION GUI / slurm interaction

--- easybuild/easyconfigs/r/RELION/relion_sbatch.sh
+++ easybuild/easyconfigs/r/RELION/relion_sbatch.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH -J Relion
-#SBATCH XXXaccountXXX
-#SBATCH XXXtimelimitXXX
 #SBATCH -n XXXmpinodesXXX
 #SBATCH -c XXXthreadsXXX
 #SBATCH -e XXXerrfileXXX
 #SBATCH -o XXXoutfileXXX
 #SBATCH XXXqosXXX
-#SBATCH XXXgpuspecXXX
-#SBATCH XXXextrasbatchXXX
+#SBATCH -t XXXextra1XXX
+#SBATCH -A XXXextra2XXX
+#SBATCH XXXextra3XXX
 
 srun XXXcommandXXX
