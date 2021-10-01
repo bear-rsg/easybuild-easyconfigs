e1e52211365aabde5bf7bce0099b6ee4e433d37a
James Carpenter <j.j.carpenter@bham.ac.uk>
2021-10-01 11:15:04 +0100

    Add exclusions re libpng dep tree

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -164,6 +164,12 @@ class EasyConfigTest(TestCase):
         # 'guilty' until proven 'innocent'
         res = False
 
+        # filter out libdrm, freetype, fontconfig, X11, Mesa if they have libpng in the versionsuffix (re ANSYSEM)
+        if dep in ['libdrm', 'freetype', 'fontconfig', 'X11', 'Mesa'] and len(dep_vars) > 1:
+            libpng1_2_58_vars = [v for v in dep_vars.keys() if v.endswith('versionsuffix: -libpng-1.2.58')]
+            if len(libpng1_2_58_vars) == 1:
+                dep_vars = dict((k, v) for (k, v) in dep_vars.items() if k != libpng1_2_58_vars[0])
+
         # filter out wrapped Java versions
         # i.e. if the version of one is a prefix of the version of the other one (e.g. 1.8 & 1.8.0_181)
         if dep == 'Java':
@@ -319,6 +325,11 @@ class EasyConfigTest(TestCase):
                       'NGSpeciesID-0.1.1.1-'])],
             # yaff requires h5py 2.10.0. LAMMPS depends on yaff.
             'h5py': [(r'2\.10\.0', [r'yaff-1\.6\.0-', r'LAMMPS-29Oct2020-'])],
+            # ANSYSEM requires libpng 1.2.58
+            'libpng': ('1.2.58', ['ANSYSEM-2021R2-', 'X11-20201008-', 'libdrm-2.4.102-', 'fontconfig-2.13.92-',
+                                   'Mesa-20.2.1-', 'freetype-2.10.3-']),
+            # ANSYSEM requires dri version of Mesa
+            'Mesa': (r'20\.2\.1.+-dri', ['ANSYSEM-']),
         }
         if dep in old_dep_versions and len(dep_vars) > 1:
             for key in list(dep_vars):
