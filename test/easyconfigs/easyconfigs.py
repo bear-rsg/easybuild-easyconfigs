790508b9ed97ce25cc5771056607f5c575bc1f4a
edmondac-admin <a.c.edmondson@bham.ac.uk>
2022-09-28 11:44:32 +0100

    Second attempt at fixing CI

--- test/easyconfigs/easyconfigs.py
+++ test/easyconfigs/easyconfigs.py
@@ -161,9 +161,9 @@ class EasyConfigTest(TestCase):
         # 'guilty' until proven 'innocent'
         res = False
 
-        # filter out libdrm, freetype, fontconfig, X11, Mesa if they have libpng in the versionsuffix (re ANSYSEM or
-        # ANSYS)
-        if dep in ['libdrm', 'freetype', 'fontconfig', 'X11', 'Mesa', 'motif'] and len(dep_vars) > 1:
+        # filter out libdrm, freetype, fontconfig, X11, Mesa, motif, libpng if they have libpng in the versionsuffix
+        # (re ANSYSEM or ANSYS)
+        if dep in ['libdrm', 'freetype', 'fontconfig', 'X11', 'Mesa', 'motif', 'libpng'] and len(dep_vars) > 1:
             libpng1_2_58_vars = [v for v in dep_vars.keys() if v.endswith('versionsuffix: -libpng-1.2.58')]
             if len(libpng1_2_58_vars) == 1:
                 dep_vars = dict((k, v) for (k, v) in dep_vars.items() if k != libpng1_2_58_vars[0])
